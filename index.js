df458a8f5a114b68cba8b51a0c7871c441482288
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-01 15:28:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+
 // Code your solution in this file.
+function lowerCaseDrivers (names) {
+  return names.map(function (drivers) {
+    return drivers.toLowerCase();
+            });
+            }
+            function nameToAttributes (Names) {
+  return Names.map(function (drivers) {
+    const driverFirst = drivers.split(' ')[0];
+    const driverLast = drivers.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };  });
+}
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+          });
+}
